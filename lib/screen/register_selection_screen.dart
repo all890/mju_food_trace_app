ade525a7c3ff59ab9254d92cb8dec451b47475d4
all890 <73221724+all890@users.noreply.github.com>
2023-09-27 22:45:46 +0700

    reconstructuring register selection page

--- lib/screen/register_selection_screen.dart
+++ lib/screen/register_selection_screen.dart
@@ -2,105 +2,188 @@
 import 'package:flutter/material.dart';
 import 'package:mju_food_trace_app/constant/constant.dart';
 import 'package:mju_food_trace_app/screen/farmer_register_screen.dart';
+import 'package:mju_food_trace_app/screen/login_screen.dart';
 import 'package:mju_food_trace_app/screen/manufacturer_register_screen.dart';
+import 'package:mju_food_trace_app/screen/user/navbar_user.dart';
 
-class RegisterSelectionScreen extends StatelessWidget {
+
+class RegisterSelectionScreen extends StatefulWidget {
   const RegisterSelectionScreen({super.key});
 
   @override
+  State<RegisterSelectionScreen> createState() => _RegisterSelectionScreenState();
+}
+
+class _RegisterSelectionScreenState extends State<RegisterSelectionScreen> {
+  final GlobalKey<ScaffoldState> _globalKey = GlobalKey<ScaffoldState>();
+
+  @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: kBackgroundColor,
-        body: Column(
-          children: [
-            GestureDetector(
-              onTap: () {
-                print("Select farmer");
-                Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(
-                    builder: (BuildContext context) {
-                      return const FarmerRegisterScreen();
-                    }
-                  )
-                );
-              },
-              child: Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height/2-50,
-                  child: Card(
-                    elevation: 10,
-                    color: Colors.green,
+    return GestureDetector(
+      onTap: () {
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      child: SafeArea(
+        child: Scaffold(
+          key: _globalKey,
+          drawer: UserNavbar(),
+          backgroundColor: kBackgroundColor,
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: IconButton(
+                    icon: Icon(Icons.menu_rounded),
+                    iconSize: 40.0,
+                    onPressed: () {
+                      _globalKey.currentState?.openDrawer();
+                    },
+                  ),
+                ),
+                SizedBox(
+                  child: SingleChildScrollView(
+                    padding: EdgeInsets.all(70),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        SizedBox(
-                          width: 180,
-                          height: 180,
+                        const Center(
                           child: Image(
-                            image: AssetImage('images/farmer_icon.png')
-                          )
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 15),
-                          child: Text(
-                            "เกษตรกร (Farmer)",
-                            style: TextStyle(
-                              fontSize: 22, fontFamily: 'Itim',color: Colors.white),
+                            image: AssetImage('images/logo.png'),
+                            width: 240,
+                            height: 240,
                           ),
-                        )
+                        ),
+                        const Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: const Text(
+                            "เลือกประเภทผู้ใช้งาน",
+                            style: TextStyle(
+                              fontFamily: 'Itim',
+                              fontSize: 24
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 10),
+                            child: SizedBox(
+                              width: 200,
+                              child: ElevatedButton(
+                                style: ButtonStyle(
+                                  shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                      RoundedRectangleBorder(
+                                        borderRadius:
+                                          BorderRadius.circular(50.0))),
+                                        backgroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 184, 184, 184))
+                                ),
+                                onPressed: () {
+                                  Navigator.of(context).pushReplacement(
+                                    MaterialPageRoute(
+                                      builder: (BuildContext context) {
+                                        return const FarmerRegisterScreen();
+                                      }
+                                    )
+                                  );
+                                },
+                                child: Row(
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.only(right: 30, top: 5, bottom: 5),
+                                      child: Image(
+                                        image: AssetImage("images/farmer_icon.png"),
+                                        width: 50,
+                                        height: 50,
+                                      ),
+                                    ),
+                                    Text("เกษตรกร",
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        fontFamily: 'Itim'
+                                      )
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 10),
+                            child: SizedBox(
+                              width: 200,
+                              child: ElevatedButton(
+                                style: ButtonStyle(
+                                  shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                      RoundedRectangleBorder(
+                                        borderRadius:
+                                          BorderRadius.circular(50.0))),
+                                        backgroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 184, 184, 184))
+                                ),
+                                onPressed: () {
+                                  Navigator.of(context).pushReplacement(
+                                    MaterialPageRoute(
+                                      builder: (BuildContext context) {
+                                        return const ManufacturerRegisterScreen();
+                                      }
+                                    )
+                                  );
+                                },
+                                child: Row(
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.only(right: 30, top: 5, bottom: 5),
+                                      child: Image(
+                                        image: AssetImage("images/factory_icon.png"),
+                                        width: 50,
+                                        height: 50,
+                                      ),
+                                    ),
+                                    Text("ผู้ผลิต",
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        fontFamily: 'Itim'
+                                      )
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 10, vertical: 25),
+                          child: InkWell(
+                            child: Text(
+                              "กลับไปยังหน้าเข้าสู่ระบบ",
+                              style: TextStyle(
+                                fontFamily: 'Itim',
+                                fontSize: 18,
+                                decoration: TextDecoration.underline
+                              ),
+                            ),
+                            onTap: () {
+                              Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(
+                                  builder: (BuildContext context) {
+                                    return const LoginScreen();
+                                  }
+                                )
+                              );
+                              print("Go to login");
+                            },
+                          ),
+                        ),
                       ],
                     ),
                   ),
                 ),
-              ),
+              ],
             ),
-            GestureDetector(
-              onTap: () {
-                print("Select manufacturer");
-                Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(
-                    builder: (BuildContext context) {
-                      return const ManufacturerRegisterScreen();
-                    }
-                  )
-                );
-              },
-              child: Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height/2,
-                  child: Card(
-                    elevation: 10,
-                    color: Colors.blue,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                          width: 180,
-                          height: 180,
-                          child: Image(
-                            image: AssetImage('images/factory_icon.png')
-                          )
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 15),
-                          child: Text(
-                            "ผู้ผลิต (Manufacturer)",
-                            style: TextStyle(
-                              fontSize: 22, fontFamily: 'Itim',color: Colors.white),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ]
+          ),
         ),
       ),
     );
